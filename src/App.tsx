4f5a8a3bd83ff780c4fb9a4aa473c3c0ef76e4a1
MohomedAskan <mohommadaskan69@gmail.com>
2026-09-19 16:26:55 +0530

    Switch to HashRouter for GitHub Pages fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { CustomCursor } from './components/CustomCursor';
 import { ScrollProgress } from './components/ScrollProgress';
 import { Navbar } from './components/Navbar';
